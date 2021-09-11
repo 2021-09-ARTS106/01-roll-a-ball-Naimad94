54afcd3857f54b0c1bf4071f0f3f703e2901b573
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2021-09-10 22:14:52 -0400

    apply input data to the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = Getcomponent<Rigidbody>();  
     }
 
-    // Update is called once per frame
-    void Update()
+    void onMove(InputValue movementValue)
     {
-        
+        // Function body (instruction for computer to see.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+    }
+
+    void fixedUpdate()
+    {
+
     }
 }
