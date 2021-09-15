a815fee9daa94ad58d5371f814ec4db8226f834f
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2021-09-15 11:39:46 -0400

    fixed error

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = Getcomponent<Rigidbody>();  
+        rb = GetComponent<Rigidbody>();  
     }
 
     void onMove(InputValue movementValue)
